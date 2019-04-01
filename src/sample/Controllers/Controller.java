f55ed0396c5390c6a0777385c784bb75f550db46
Motosbor <Lotsmanenkorr@mail.ru>
2019-04-01 17:54:24 +0400

    11

--- src/sample/Controllers/Controller.java
+++ src/sample/Controllers/Controller.java
@@ -1,20 +1,28 @@
 package sample.Controllers;
 
+import java.awt.event.ActionEvent;
 import java.beans.EventHandler;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import sample.Helpers.Loader;
 import sample.WorkDB.ConnectorToDB;
 import sample.WorkDB.WorkerDB;
 
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML
     private ResourceBundle resources;
@@ -66,17 +74,35 @@ public class Controller {
             }
         });
 
-        registryButton.setOnAction(event -> {
-
-            registryButton.getScene().getWindow().hide();
-
-            new Loader("/sample/View/signUp.fxml");
-
-        });
+//        registryButton.setOnAction(event -> {
+//
+//            registryButton.getScene().getWindow().hide();
+//
+//            new Loader("/sample/View/signUp.fxml");
+//
+//        });
 
     }
 
     private void loginUser(String login, String password) {
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void changeButt(javafx.event.ActionEvent actionEvent) throws IOException {
+
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("/sample/View/signUp.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
     }
 }
 
